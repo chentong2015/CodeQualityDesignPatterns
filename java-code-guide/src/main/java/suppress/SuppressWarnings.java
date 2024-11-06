545ac3e774e621a6e8cc67eaa502ee892a0b93d5
tchen <chen2015tong@gmail.com>
2024-11-06 16:18:43 +0100

    update suppress warnings

--- java-code-guide/src/main/java/suppress/SuppressWarnings.java
+++ java-code-guide/src/main/java/suppress/SuppressWarnings.java
@@ -1,4 +1,4 @@
-package suppress_warnings;
+package suppress;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,12 +10,13 @@ import java.util.List;
 // 2. 添加在class上的抑制名称会作用到它的全部方法, 但应该减小抑制的Scope
 // 3. 不应该抑制废弃API的提示, 特别是对于即将废弃的API
 
-@SuppressWarnings("deprecation")
+// @SuppressWarnings("deprecation")
+// @SuppressWarnings("DeprecatedIsStillUsed")
 // @SuppressWarnings("removal")
-public class SuppressWarningsDemo {
+public class SuppressWarnings {
 
     // TODO. 当且仅当在使用泛型集合时无法确定实例类型时，才不指定并抑制提示 !!
-    @SuppressWarnings({"unchecked", "rawtypes"})
+    @java.lang.SuppressWarnings({"unchecked", "rawtypes"})
     public void testChild() {
         // 1. rawtypes warning
         // 泛型集合在使用时通常需要特化泛型，否则会出现rawtypes的提示
