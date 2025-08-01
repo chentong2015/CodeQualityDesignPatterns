2106392280987251909d7fe0d5c05b29ab289536
chent5 <chen2015tong@gmail.com>
2025-08-01 18:19:24 +0200

    update switch case demo

---
+++ java-code-guide/src/main/java/switch_case/MyEnum.java
@@ -0,0 +1,8 @@
+package switch_case;
+
+public enum MyEnum {
+    ALL,
+    UPDATE,
+    DELETE,
+    WARNING
+}
--- java-code-guide/src/main/java/switch_case/SwitchCaseDemo.java
+++ java-code-guide/src/main/java/switch_case/SwitchCaseDemo.java
@@ -22,11 +22,20 @@ public class SwitchCaseDemo {
             // 删除case时不能连带删除执行语句块，否则前面case执行后续错误逻辑
                 System.out.println("case invoked Three");
                 break;
-            case 7:
-                System.out.println("case invoked Four");
-                break;
             default:
                 System.out.println("default");
+        }
+    }
+
+    // TODO. 使用新版本的Switch Case需要考虑回归问题
+    // 保证向下兼容的所有JDK版本都能运行
+    private static void testSwitchCase2(MyEnum myEnum) {
+        switch (myEnum) {
+            case ALL -> System.out.println("case all invoked");
+            case UPDATE -> System.out.println("case update invoked");
+            case DELETE -> System.out.println("case delete invoked");
+            case  WARNING -> System.out.println("case warning invoked");
+            default -> System.out.println("default");
         }
     }
 }
